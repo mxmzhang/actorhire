7453acd251056c6fd05e13c8acea0e3e71b39e2e
mxmzhang <144863474+mxmzhang@users.noreply.github.com>
2024-09-05 10:29:00 -0400

    add login html pages, test postgres

--- index.js
+++ index.js
@@ -12,7 +12,24 @@ var hbs = require('hbs')
 hbs.registerPartials(__dirname + '/views/partials', function (err) {});
 app.set('view engine','hbs')
 
+const { Pool } = require('pg');
+const pool = new Pool({
+    connectionString: process.env.DATABASE_URL,
+    ssl: {
+        rejectUnauthorized: false
+    }
+});
+
 app.get('/', function(req, res) {
+    console.log("here!")
+    pool.query("CREATE TABLE IF NOT EXISTS users (user_id INT GENERATED ALWAYS IDENTITY, username VARCHAR(70), password VARCHAR(70), name VARCHAR(70)", 
+        function(err, result) {
+            console.log('done')
+            if(err) {
+                console.log('error')
+                console.log(err)
+            }
+    })
     res.render('index')
 })
 
@@ -23,6 +40,14 @@ app.get('/account', function(req, res) {
 app.get('/hire', function(req, res) {
     res.render('index')
 })
+
+app.get('/login', function(req, res) {
+    res.render('index')
+})
+
+app.get('/form-response', function(req, res) {
+
+}) 
 
 app.listen(PORT, () => {
     console.log(`server started on port ${PORT}`);
