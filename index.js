162052987a1234ace5265a57f013c212bfe026b5
mxmzhang <144863474+mxmzhang@users.noreply.github.com>
2024-09-06 18:13:44 -0400

    added edit table function

--- index.js
+++ index.js
@@ -40,7 +40,7 @@ app.get('/account', function(req, res) {
 })
 
 function editActorsTable(req, res, next) {
-    pool.query(`ALTER TABLE actors ADD times integer[]`,
+    pool.query(`ALTER TABLE actors ADD times INT[]`,
         function(err, results) {
             if (err) {
                 console.error("alter actors table ", err);
@@ -54,8 +54,8 @@ function editActorsTable(req, res, next) {
 
 function insertInitialActors(req, res, next) {
     pool.query(`INSERT INTO actors (fName, lName, descrip, times) VALUES
-        ('Sarah', 'Zheng', 'I graduated from Boston University with a Bachelor''s in drama. I''ve had 6+ years of experience in acting, including a stint in Sponge Bob the Musical, and I specialize in break-ups, crazy cat ladies, and distressed friend', [9,10,11,12,13,14,15]),
-        ('Jonah', 'Higgins', 'Hello! I''m Jonah, and I''m a budding Off-Broadway actor. You may recognize me at 00:45:02 of Legally Blonde. I specialize in dramatic distractions and scaring people.', [11,12,13,14,15,18,19,20])`,
+        ('Sarah', 'Zheng', 'I graduated from Boston University with a Bachelor''s in drama. I''ve had 6+ years of experience in acting, including a stint in Sponge Bob the Musical, and I specialize in break-ups, crazy cat ladies, and distressed friend', ARRAY[9,10,11,12,13,14,15]),
+        ('Jonah', 'Higgins', 'Hello! I''m Jonah, and I''m a budding Off-Broadway actor. You may recognize me at 00:45:02 of Legally Blonde. I specialize in dramatic distractions and scaring people.', ARRAY[11,12,13,14,15,18,19,20])`,
         function(err, results) {
             if (err) {
                 console.error("insert actors ", err);
@@ -68,7 +68,7 @@ function insertInitialActors(req, res, next) {
 }
 
 app.get('/hire', function(req, res) {
-    pool.query("SELECT * FROM actors", insertInitialActors, function(err, results) {
+    pool.query("SELECT * FROM actors", editActorsTable, insertInitialActors, function(err, results) {
         if (err) {
             console.error("getting actors ", err)
             return;
